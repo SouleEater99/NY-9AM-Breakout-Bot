842de018fa656bc85af5be93a8cb5b889e562fe3
Abdelali El Maimouni <rimox.midox2011@gmail.com>
2025-02-01 08:13:47 -0500

    ==> edit setters

--- set_litefinance.sh
+++ set_litefinance.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 cp -r ./*mqh    ~/.wine/drive_c/Program\ Files/LiteFinance\ MT5\ Terminal/MQL5/Include
-cp -r ./*mqh    ~/.wine/drive_c/Program\ Files/LiteFinance\ MT5\ Terminal/MQL5/Experts
+cp -r ./*mq5    ~/.wine/drive_c/Program\ Files/LiteFinance\ MT5\ Terminal/MQL5/Experts
 
 echo "Files seted"
