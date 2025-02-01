88c8d238fa25fbe6ffac2594239398dd8d3daa49
abdo-ox <abderrahim.jari6@gmail.com>
2025-02-01 13:03:05 +0100

    add the get and set script for moving from repo to exness directory

---
+++ get.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+cp -r ~/.wine/drive_c/Program\ Files/MetaTrader\ 5\ EXNESS/MQL5/Include/MyGlobals.mqh .
+cp -r ~/.wine/drive_c/Program\ Files/MetaTrader\ 5\ EXNESS/MQL5/Include/OrderPart.mqh .
+cp -r ~/.wine/drive_c/Program\ Files/MetaTrader\ 5\ EXNESS/MQL5/Experts/main.mq5 .
+echo "Files seted"
---
+++ set.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+cp -r ./*mqh    ~/.wine/drive_c/Program\ Files/MetaTrader\ 5\ EXNESS/MQL5/Include
+cp -r ./*mq5    ~/.wine/drive_c/Program\ Files/MetaTrader\ 5\ EXNESS/MQL5/Experts
+echo "Files seted"
